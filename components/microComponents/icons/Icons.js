ea00de5b04e4456d73cb64b0fd489ce774ce44f2
Amirhossein <amirhosseinseddigh@gmail.com>
2022-02-21 16:46:40 +0330

    icons refactored

--- components/microComponents/icons/Icons.js
+++ components/microComponents/icons/Icons.js
@@ -1,6 +1,102 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
+
+export function Home() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="40" height="40" viewBox="0 0 40 40">
+            <defs>
+                <filter id="Path_515" x="-2.668" y="0.12" width="45.337" height="45.648" filterUnits="userSpaceOnUse">
+                    <feOffset dy="3" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="3" result="blur" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+                <clipPath id="clip-Artboard_1">
+                    <rect width="40" height="40" />
+                </clipPath>
+            </defs>
+            <g id="Artboard_1" data-name="Artboard – 1" clipPath="url(#clip-Artboard_1)">
+                <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Path_515)">
+                    <path id="Path_515-2" data-name="Path 515" d="M-105.9,158.259V145.971a1.172,1.172,0,0,1,.37-.85l10.877-10.345a1.3,1.3,0,0,1,1.744-.03l11.446,10.14a1.171,1.171,0,0,1,.4.881l-.011,12.492a1.231,1.231,0,0,1-1.258,1.2h-5.849a1.231,1.231,0,0,1-1.259-1.2v-7.488a1.231,1.231,0,0,0-.39-.869,1.231,1.231,0,0,0-.893-.331l-5.269.1a1.229,1.229,0,0,0-1.232,1.2v7.387a1.231,1.231,0,0,1-1.259,1.2h-6.161a1.23,1.23,0,0,1-1.256-1.2Z" transform="translate(113.48 -126.94)" fill="none" stroke="#fff" strokeMiterlimit="10" strokeWidth="2.5" />
+                </g>
+            </g>
+        </svg>
+    )
+}
+export function NavRequest() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="40" height="40" viewBox="0 0 40 40">
+            <defs>
+                <clipPath id="clip-Artboard_1">
+                    <rect width="40" height="40" />
+                </clipPath>
+            </defs>
+            <g id="Artboard_1" data-name="Artboard – 1" clipPath="url(#clip-Artboard_1)">
+                <path id="assignment-o" d="M19.525,2.462a2.376,2.376,0,0,1,1.746.715,2.313,2.313,0,0,1,.716,1.689V24.45a2.31,2.31,0,0,1-.715,1.689,2.377,2.377,0,0,1-1.747.715H2.462a2.376,2.376,0,0,1-1.746-.715A2.313,2.313,0,0,1,0,24.45V4.867A2.31,2.31,0,0,1,.715,3.178a2.377,2.377,0,0,1,1.747-.715h5.1A3.756,3.756,0,0,1,8.9.688a3.522,3.522,0,0,1,4.18,0,3.761,3.761,0,0,1,1.345,1.775Zm-8.531,0a1.175,1.175,0,0,0-1.2,1.2,1.2,1.2,0,1,0,2.405,0,1.175,1.175,0,0,0-1.2-1.2ZM19.525,24.45V4.867h-2.4V8.531H4.867V4.867h-2.4V24.45H19.526Z" transform="translate(9.006 6.573)" fill="#fff" />
+            </g>
+        </svg>
+    )
+}
+
+export function Contact() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="40" height="40" viewBox="0 0 40 40">
+            <defs>
+                <filter id="bx-support" x="-2.428" y="0.573" width="44.856" height="44.854" filterUnits="userSpaceOnUse">
+                    <feOffset dy="3" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="3" result="blur" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+                <clipPath id="clip-Artboard_3">
+                    <rect width="40" height="40" />
+                </clipPath>
+            </defs>
+            <g id="Artboard_1" data-name="Artboard – 1" clipPath="url(#clip-Artboard_1)">
+                <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#bx-support)">
+                    <path id="bx-support-2" data-name="bx-support" d="M16.427,3A13.442,13.442,0,0,0,3,16.427V21.99a2.6,2.6,0,0,0,2.685,2.494H7.028a1.343,1.343,0,0,0,1.343-1.343v-6.9a1.343,1.343,0,0,0-1.343-1.343H5.809a10.726,10.726,0,0,1,21.237,0h-1.22a1.343,1.343,0,0,0-1.343,1.343v8.248A2.688,2.688,0,0,1,21.8,27.169H19.113V25.826H13.742v4.028H21.8a5.376,5.376,0,0,0,5.371-5.371,2.6,2.6,0,0,0,2.685-2.493V16.427A13.442,13.442,0,0,0,16.427,3Z" transform="translate(3.57 3.57)" fill="#fff" />
+                </g>
+            </g>
+        </svg>
+    )
+}
+
+export function Profile() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="40" height="40" viewBox="0 0 40 40">
+            <defs>
+                <filter id="Path_413" x="6.11" y="5.443" width="27.779" height="27.779" filterUnits="userSpaceOnUse">
+                    <feOffset dy="3" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="3" result="blur" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+                <filter id="Path_414" x="-2.446" y="0.554" width="44.893" height="44.893" filterUnits="userSpaceOnUse">
+                    <feOffset dy="3" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="3" result="blur-2" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur-2" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+                <clipPath id="clip-Artboard_4">
+                    <rect width="40" height="40" />
+                </clipPath>
+            </defs>
+            <g id="Artboard_1" data-name="Artboard – 1" clipPath="url(#clip-Artboard_1)">
+                <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Path_413)">
+                    <path id="Path_413-2" data-name="Path 413" d="M21.779,12.39A4.89,4.89,0,1,1,16.89,7.5,4.89,4.89,0,0,1,21.779,12.39Zm-2.445,0A2.445,2.445,0,1,1,16.89,9.945,2.445,2.445,0,0,1,19.334,12.39Z" transform="translate(3.11 3.94)" fill="#fff" fillRule="evenodd" />
+                </g>
+                <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Path_414)">
+                    <path id="Path_414-2" data-name="Path 414" d="M14.946,1.5A13.446,13.446,0,1,0,28.393,14.946,13.446,13.446,0,0,0,14.946,1.5Zm-11,13.446a10.957,10.957,0,0,0,2.332,6.774,11,11,0,0,1,17.418-.1A11,11,0,1,0,3.945,14.946Zm11,11a10.956,10.956,0,0,1-6.933-2.459,8.56,8.56,0,0,1,13.968-.083A10.957,10.957,0,0,1,14.946,25.948Z" transform="translate(5.05 5.05)" fill="#fff" fillRule="evenodd" />
+                </g>
+            </g>
+        </svg>
+    )
+}
 export function Request() {
     return (
         <Link href='/requests'>
@@ -39,3 +135,100 @@ export function Menu() {
         </svg>
     )
 }
+
+export function Mobile() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="27" height="40.5" viewBox="0 0 27 40.5">
+            <defs>
+                <filter id="Path_446" x="0" y="0" width="27" height="40.5" filterUnits="userSpaceOnUse">
+                    <feOffset dy="2" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="0.75" result="blur" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+                <filter id="Path_447" x="9" y="27" width="9" height="9" filterUnits="userSpaceOnUse">
+                    <feOffset dy="2" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="0.75" result="blur-2" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur-2" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+            </defs>
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Path_446)">
+                <path id="Path_446-2" data-name="Path 446" d="M24.75,2.25H11.25A2.25,2.25,0,0,0,9,4.5v27a2.25,2.25,0,0,0,2.25,2.25h13.5A2.25,2.25,0,0,0,27,31.5V4.5A2.25,2.25,0,0,0,24.75,2.25ZM11.25,0a4.5,4.5,0,0,0-4.5,4.5v27a4.5,4.5,0,0,0,4.5,4.5h13.5a4.5,4.5,0,0,0,4.5-4.5V4.5A4.5,4.5,0,0,0,24.75,0Z" transform="translate(-4.5 0.25)" fillRule="evenodd" />
+            </g>
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Path_447)">
+                <path id="Path_447-2" data-name="Path 447" d="M18,31.5a2.25,2.25,0,1,0-2.25-2.25A2.25,2.25,0,0,0,18,31.5Z" transform="translate(-4.5 0.25)" fillRule="evenodd" />
+            </g>
+        </svg>);
+}
+
+export function Phone() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="39.842" height="39.829" viewBox="0 0 39.842 39.829">
+            <defs>
+                <filter id="phone" x="0" y="0" width="39.842" height="39.829" filterUnits="userSpaceOnUse">
+                    <feOffset dy="2" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="0.75" result="blur" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+            </defs>
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#phone)">
+                <path id="phone-2" data-name="phone" d="M34.63,38.7H34.4C7.7,37.169,3.907,14.639,3.377,7.764a4.076,4.076,0,0,1,3.75-4.389h7.487a2.718,2.718,0,0,1,2.528,1.711l2.064,5.083a2.718,2.718,0,0,1-.6,2.935l-2.894,2.922a12.732,12.732,0,0,0,10.3,10.327l2.948-2.922a2.718,2.718,0,0,1,2.948-.557l5.122,2.052a2.718,2.718,0,0,1,1.673,2.527v7.174A4.076,4.076,0,0,1,34.63,38.7ZM7.454,6.093A1.359,1.359,0,0,0,6.095,7.451V7.56C6.72,15.6,10.728,34.627,34.549,35.986A1.359,1.359,0,0,0,35.99,34.71V27.453L30.867,25.4l-3.9,3.872-.652-.082C14.494,27.71,12.89,15.888,12.89,15.766l-.082-.652,3.859-3.9L14.628,6.093Z" transform="translate(-1.11 -3.13)" />
+            </g>
+        </svg>
+    )
+}
+
+export function WhatsApp() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="39.5" height="39.5" viewBox="0 0 39.5 39.5">
+            <defs>
+                <filter id="Path_451" x="9.19" y="8.744" width="22.095" height="21.31" filterUnits="userSpaceOnUse">
+                    <feOffset dy="2" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="0.75" result="blur" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+                <filter id="Path_452" x="0" y="0" width="39.5" height="39.5" filterUnits="userSpaceOnUse">
+                    <feOffset dy="2" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="0.75" result="blur-2" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur-2" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+            </defs>
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Path_451)">
+                <path id="Path_451-2" data-name="Path 451" d="M15.245,10.915c-.334-.88-.72-.817-.977-.827s-.482,0-.829,0a1.7,1.7,0,0,0-1.2.5,4.828,4.828,0,0,0-1.68,3.554,8.337,8.337,0,0,0,1.629,4.544c.21.3,2.865,4.915,7.278,6.814s4.436,1.32,5.247,1.278a4.464,4.464,0,0,0,3.053-1.949,3.548,3.548,0,0,0,.335-2.048c-.1-.185-.393-.3-.825-.536s-2.559-1.36-2.957-1.52-.69-.242-1,.181-1.192,1.365-1.46,1.645-.527.306-.96.072a12.064,12.064,0,0,1-3.462-2.278A13.093,13.093,0,0,1,15.1,17.255c-.241-.443,0-.669.228-.876.205-.187.46-.49.69-.734a2.949,2.949,0,0,0,.468-.706.787.787,0,0,0-.008-.759c-.1-.221-.9-2.383-1.236-3.263Z" transform="translate(0.88 -1.09)" fillRule="evenodd" />
+            </g>
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Path_452)">
+                <path id="Path_452-2" data-name="Path 452" d="M20.068,4.75A14.652,14.652,0,0,1,30.457,9.018a14.457,14.457,0,0,1,0,20.562,14.655,14.655,0,0,1-10.389,4.269,14.806,14.806,0,0,1-7.1-1.812l-.939-.516-1.02.324-4.8,1.526,1.513-4.464L8.093,27.8l-.607-1A14.453,14.453,0,0,1,9.679,9.02,14.647,14.647,0,0,1,20.068,4.75m0-2.5A17.117,17.117,0,0,0,2.886,19.3a16.873,16.873,0,0,0,2.466,8.8L2.25,37.25l9.513-3.022A17.21,17.21,0,0,0,37.25,19.3,17.116,17.116,0,0,0,20.068,2.25Z" transform="translate(0 -2)" />
+            </g>
+        </svg>
+
+    )
+}
+
+export function Instagram() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="39.5" height="39.499" viewBox="0 0 39.5 39.499">
+            <defs>
+                <filter id="instagram" x="0" y="0" width="39.5" height="39.499" filterUnits="userSpaceOnUse">
+                    <feOffset dy="2" input="SourceAlpha" />
+                    <feGaussianBlur stdDeviation="0.75" result="blur" />
+                    <feFlood floodOpacity="0.161" />
+                    <feComposite operator="in" in2="blur" />
+                    <feComposite in="SourceGraphic" />
+                </filter>
+            </defs>
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#instagram)">
+                <path id="instagram-2" data-name="instagram" d="M20.5,3c-4.753,0-5.348.021-7.214.1a12.879,12.879,0,0,0-4.248.814A8.943,8.943,0,0,0,3.918,9.037,12.9,12.9,0,0,0,3.1,13.285C3.02,15.151,3,15.746,3,20.5s.02,5.348.1,7.214a12.879,12.879,0,0,0,.814,4.248A8.94,8.94,0,0,0,9.038,37.08a12.9,12.9,0,0,0,4.248.814c1.867.084,2.463.1,7.214.1s5.348-.021,7.214-.1a12.879,12.879,0,0,0,4.248-.814,8.941,8.941,0,0,0,5.119-5.119,12.9,12.9,0,0,0,.814-4.248c.084-1.867.1-2.463.1-7.214s-.021-5.348-.1-7.214a12.878,12.878,0,0,0-.814-4.248,8.94,8.94,0,0,0-5.119-5.119A12.9,12.9,0,0,0,27.714,3.1C25.846,3.021,25.252,3,20.5,3m0,3.153c4.672,0,5.225.017,7.07.1a9.722,9.722,0,0,1,3.251.6,5.793,5.793,0,0,1,3.321,3.321,9.711,9.711,0,0,1,.6,3.249c.084,1.847.1,2.4.1,7.072s-.017,5.225-.1,7.07a9.783,9.783,0,0,1-.6,3.251,5.793,5.793,0,0,1-3.321,3.321,9.711,9.711,0,0,1-3.249.6c-1.844.084-2.4.1-7.072.1s-5.227-.017-7.07-.1a9.783,9.783,0,0,1-3.251-.6A5.793,5.793,0,0,1,6.857,30.82a9.65,9.65,0,0,1-.6-3.249c-.084-1.847-.1-2.4-.1-7.072s.017-5.225.1-7.07a9.722,9.722,0,0,1,.6-3.251A5.475,5.475,0,0,1,8.166,8.166a5.446,5.446,0,0,1,2.012-1.309,9.651,9.651,0,0,1,3.249-.6c1.847-.084,2.4-.1,7.072-.1m0,20.178A5.833,5.833,0,1,1,26.332,20.5,5.832,5.832,0,0,1,20.5,26.332m0-14.818A8.986,8.986,0,1,0,29.484,20.5,8.986,8.986,0,0,0,20.5,11.514m11.441-.355a2.1,2.1,0,1,1-2.1-2.1,2.1,2.1,0,0,1,2.1,2.1" transform="translate(-0.75 -2.75)" fillRule="evenodd" />
+            </g>
+        </svg>
+    )
+}
+
