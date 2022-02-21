37b4123b8ba5e9fab383e1b0b186960a10f0ef4e
Amirhossein <amirhosseinseddigh@gmail.com>
2022-02-21 12:37:56 +0330

    console.log removed

--- components/services/servicesItems/ServicesChildren.js
+++ components/services/servicesItems/ServicesChildren.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { pendingRequest } from '../../../lib/requests';
 import RadioBtn from '../../microComponents/radioButton/RadioBtn';
 import styles from './ServicesItems.module.scss';
@@ -9,25 +9,24 @@ export default function ServicesChildren(props) {
     const title = service.title;
     const price = service.price;
     const prescription = props.prescription;
-    
+
     const update = props.update;
 
     const [pending, setPending] = useState(service.pending);
     useEffect(() => {
         setPending(service.pending);
-    },[service])
-    
+    }, [service])
+
     const handleClick = async (event) => {
         event.preventDefault();
         const serviceId = event.currentTarget.id;
         const requestAction = pending ? 'delete' : 'post';
         const resData = await pendingRequest(serviceId, prescription, requestAction);
-        console.log(resData, prescription);
         update();
     }
     return (
         <div id={id} onClick={handleClick} className={styles.container + ' global-container'}>
-            <RadioBtn checked={pending}/>
+            <RadioBtn checked={pending} />
             <div className={styles.childBox}>
                 <div className={styles.servicesTitle}>{title}</div>
                 <div className={styles.servicesPrice}>
