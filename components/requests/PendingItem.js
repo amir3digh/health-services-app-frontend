2b5845c994c8a47c9657d6e064e215a544c3b7c6
Amirhossein <amirhosseinseddigh@gmail.com>
2022-03-11 23:51:34 +0330

    paid items added to request page

--- components/requests/PendingItem.js
+++ components/requests/PendingItem.js
@@ -24,12 +24,12 @@ export default function PendingItem(props) {
                 <div>{title}</div>
                 <div className="subtitle">قیمت: {stylizePrice(price)} تومان</div>
             </div>
-            <div onClick={handleDeleteClick} className='button'>
+            {deletePending && <div onClick={handleDeleteClick} className='button'>
                 <svg id="trash-empty" xmlns="http://www.w3.org/2000/svg" width="10.516" height="11.382" viewBox="0 0 10.516 11.382">
                     <path id="Path_533" data-name="Path 533" d="M12.679,6.3V5.7a1.184,1.184,0,0,0-1.168-1.2H8.005A1.184,1.184,0,0,0,6.837,5.7v.6H5.084a.6.6,0,0,0,0,1.2h.584v6.589a1.775,1.775,0,0,0,1.753,1.8h4.674a1.775,1.775,0,0,0,1.753-1.8V7.5h.584a.6.6,0,0,0,0-1.2Zm-1.168-.6H8.005v.6h3.505Zm1.168,1.8H6.837v6.589a.592.592,0,0,0,.584.6h4.674a.592.592,0,0,0,.584-.6Z" transform="translate(-4.5 -4.5)" fillRule="evenodd" />
                 </svg>
                 <span>حذف</span>
-            </div>
+            </div>}
         </div>
     )
 }
--- pages/requests/index.js
+++ pages/requests/index.js
@@ -1,6 +1,6 @@
 import PendingItem from '../../components/requests/PendingItem';
 import { useEffect, useState } from 'react';
-import { pendingRequest } from '../../lib/requests';
+import { paidServicesRequest, pendingRequest } from '../../lib/requests';
 import styles from './RequestsPage.module.scss';
 import Link from 'next/link';
 import Layout from '../../components/layout/Layout';
@@ -10,19 +10,26 @@ export function getStaticProps() {
 }
 
 export default function RequestPage() {
-    const [pending, setPending] = useState([]);
+    const [show, setShow] = useState('pending');
 
     useEffect(() => {
         updatePending();
+        updatePaid();
     }, []);
-
-    const [show, setShow] = useState('pending');
-
+    const [pending, setPending] = useState([]);
     const updatePending = async () => {
         const response = await pendingRequest('', null, 'get');
         if (response.status === 'ok') {
             const result = response.result;
             setPending(result);
+        }
+    }
+    const [paid, setPaid] = useState([]);
+    const updatePaid = async () => {
+        const response = await paidServicesRequest();
+        if (response.status === 'ok') {
+            const result = response.result;
+            setPaid(result);
         }
     }
     const deletePending = async (serviceId, pendingServiceId) => {
@@ -32,22 +39,20 @@ export default function RequestPage() {
         }
     }
 
+
     return (
         <Layout name='requests'>
             <div className={styles.toggleBtnContainer}>
-                <div className={styles.toggleBtn + ' ' + (show === 'pending' && styles.enabled)}>
+                <button onClick={() => setShow('pending')} className={styles.toggleBtn + ' ' + (show === 'pending' && styles.enabled)}>
                     درخواست های فعال
-                </div>
-                <div className={styles.toggleBtn + ' ' + (show === 'paid' && styles.enabled)}>
-                    درخواست های در انتظار
-                </div>
-                <div className={styles.toggleBtn + ' ' + (show === 'old' && styles.enabled)}>
+                </button>
+                <button onClick={() => setShow('paid')} className={styles.toggleBtn + ' ' + (show === 'paid' && styles.enabled)}>
                     درخواست های پیشین
-                </div>
+                </button>
             </div>
             <div className="global-container">
-                <div className={styles.title}>لیست درخواست های فعال</div>
-                {pending.map(el => (
+                <div className={styles.title}>لیست درخواست های {show === 'pending' ? 'فعال' : 'پیشین'}</div>
+                {show === 'pending' && pending.map(el => (
                     <PendingItem
                         key={el.pending_service_id}
                         title={el.title}
@@ -57,12 +62,20 @@ export default function RequestPage() {
                         deletePending={deletePending}
                     />
                 ))}
-                {pending.length ?
+                {(show === 'pending' && pending.length) ?
                     <Link href={'services/pending'}>
                         <a className={styles.saveBtn}>ثبت نهایی درخواست های فعال</a>
                     </Link>
                     : ''
                 }
+                {show === 'paid' && paid.map(el => (
+                    <PendingItem
+                        key={el.paid_service_id}
+                        title={el.title}
+                        serviceId={el.service_id}
+                        price={el.price}
+                    />
+                ))}
             </div>
         </Layout>
     )
